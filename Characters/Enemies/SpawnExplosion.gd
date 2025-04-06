ff4ef8195459c9310365b3c589d3468722c608e8
Thapa-3097523 <143840451+asthathapa4@users.noreply.github.com>
2025-04-06 18:18:09 -0500

    Update
    fixed player and npc, deleted temp art

--- Characters/Enemies/SpawnExplosion.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatedSprite2D
-
-
-func _ready() -> void:
-	play("default")
-
-
-func _on_SpawnExplosion_animation_finished() -> void:
-	queue_free()
--- game.gd
+++ game.gd
@@ -2,14 +2,14 @@ extends Node2D
 
 @onready var dialogue_window = $DialogueWindow
 @onready var player = $Player
-@onready var goblin = $Goblin
+@onready var npc = $NPC
 
 var battle = preload("res://Battle Scene/Battle.tscn") # THIS WILL BE USED TO SWITCH OVER TO BATTLE SCENE (TBA)
 var can_interact = false  # Tracks if the player is in range of the Goblin
 
 func _ready() -> void:
 	_hide_dialogue_window()  # Hide everything initially
-	goblin.connect("player_near", Callable(self, "_on_goblin_player_near"))  # Connect Goblin signal to Game
+	npc.connect("player_near", Callable(self, "_on_npc_player_near"))  # Connect Goblin signal to Game
 
 func _process(_delta: float) -> void:
 	if can_interact and Input.is_action_just_pressed("interact"):  # 'E' should be mapped to 'interact' in InputMap
@@ -20,12 +20,12 @@ func _process(_delta: float) -> void:
 		#get_parent().addchild(battleTemp)
 		#queue_free()
 
-func _on_goblin_player_near(state: bool) -> void:
+func _on_npc_player_near(state: bool) -> void:
 	can_interact = state
 	if state:
-		print("Player is near Goblin. Press 'E' to interact.")  # Debugging line
+		print("Player is near NPC. Press 'E' to interact.")  # Debugging line
 	else:
-		print("Player left Goblin.")  # Debugging line
+		print("Player left NPC.")  # Debugging line
 
 func _hide_dialogue_window() -> void:
 	dialogue_window.visible = false  # Hide the main window
