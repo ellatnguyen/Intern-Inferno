08884d909224bceb77e2dfbaf5d930147c5962c3
ellatnguyen <ellanguyen37@gmail.com>
2025-04-22 14:58:23 -0500

    Hotkey Functionality + Reversed Compliance Bar Animation
    O = INT, P = PER, L = Leave
    Also compliance bar fills up instead of drops

--- dialogue_window.gd
+++ dialogue_window.gd
@@ -1,10 +1,11 @@
 extends Control
 
-@onready var dialogue_text = $CanvasLayer/DialogueText
+@onready var dialogue_text = $CanvasLayer/DialogueBoxesWithDarkenedBackground/DialogueText
+#@onready var dialogue_text = $CanvasLayer/DialogueText
 @onready var int_button = $CanvasLayer/INT_Button
 @onready var per_button = $CanvasLayer/PER_Button
 @onready var leave_button = $CanvasLayer/LEAVE_Button
-@onready var dialogue_window = $DialogueWindow
+#@onready var dialogue_window = $DialogueWindow
 @onready var health_bar = $"CanvasLayer/PrototypeEnemy/ComplianceBar"
 
 var current_int_line = 0
@@ -24,9 +25,9 @@ func _ready():
 	
 	print("Buttons found successfully!")
 
-	int_button.pressed.connect(_on_int_button_pressed)
-	per_button.pressed.connect(_on_per_button_pressed)
-	leave_button.pressed.connect(_on_leave_button_pressed)
+	int_button.visible = true
+	per_button.visible = true
+	leave_button.visible = true
 
 	var file = FileAccess.open("res://dialogues.txt", FileAccess.READ)
 	if file:
@@ -37,22 +38,62 @@ func _ready():
 	update_dialogue()
 	update_health_bar()
 
+#func _unhandled_input(event):
+	#if event.is_action_pressed("interact"):  # Press E
+		#start_battle()
 func _unhandled_input(event):
-	if event.is_action_pressed("interact"):  # Press E
+	if not self.visible:
+		return  # Only allow inputs during battle
+
+	if event.is_action_pressed("battle_per"):
+		_on_per_button_pressed()
+
+	elif event.is_action_pressed("battle_int"):
+		_on_int_button_pressed()
+
+	elif event.is_action_pressed("battle_leave"):
+		_on_leave_button_pressed()
+
+	elif event.is_action_pressed("interact"):
 		start_battle()
 
+
+#func start_battle():
+	#print("Battle started!")
+	#enemy_health = MAX_HEALTH
+	#current_int_line = 0
+	#current_per_line = 0
+	#current_dialogue = []
+	#update_dialogue()
+	#update_health_bar()
+	#
+	#self.visible = true
+	#for child in get_children():
+		#child.visible = true
+		
 func start_battle():
 	print("Battle started!")
 	enemy_health = MAX_HEALTH
 	current_int_line = 0
 	current_per_line = 0
-	current_dialogue = []
+
+	current_dialogue = int_dialogue  # <--- This is the missing line
+
 	update_dialogue()
 	update_health_bar()
 	
+	int_button.visible = true
+	per_button.visible = true
+	leave_button.visible = true
+
+	int_button.disabled = false
+	per_button.disabled = false
+	leave_button.disabled = false
+
 	self.visible = true
 	for child in get_children():
 		child.visible = true
+
 
 func parse_dialogue(dialogue_content):
 	var lines = dialogue_content.split("\n")
@@ -87,13 +128,23 @@ func update_dialogue():
 	else:
 		dialogue_text.text = "No dialogue available."
 
+#func _on_int_button_pressed():
+	#print("INT Button Pressed!")
+	#current_dialogue = int_dialogue
+	#current_int_line += 1
+	#if current_int_line < int_dialogue.size():
+		#update_dialogue()
+		#decrease_enemy_health(1)  # INT reduces by 1
+	#else:
+		#dialogue_text.text = "End of INT dialogue."
+		
 func _on_int_button_pressed():
 	print("INT Button Pressed!")
 	current_dialogue = int_dialogue
-	current_int_line += 1
 	if current_int_line < int_dialogue.size():
 		update_dialogue()
-		decrease_enemy_health(1)  # INT reduces by 1
+		decrease_enemy_health(1)
+		current_int_line += 1  # Move this after the update
 	else:
 		dialogue_text.text = "End of INT dialogue."
 
