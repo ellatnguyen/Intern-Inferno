f30976a5d7ec11bec6d894e938070ef13e4d67f9
ellatnguyen <ellanguyen37@gmail.com>
2025-04-23 13:46:28 -0500

    Instantiated Dialogue Battle into Map.tscn
    NOTE: DOES NOT HAVE DIFFERENT ENEMIES YET

--- dialogue_window.gd
+++ dialogue_window.gd
@@ -1,11 +1,9 @@
 extends Control
 
 @onready var dialogue_text = $CanvasLayer/DialogueBoxesWithDarkenedBackground/DialogueText
-#@onready var dialogue_text = $CanvasLayer/DialogueText
 @onready var int_button = $CanvasLayer/INT_Button
 @onready var per_button = $CanvasLayer/PER_Button
 @onready var leave_button = $CanvasLayer/LEAVE_Button
-#@onready var dialogue_window = $DialogueWindow
 @onready var health_bar = $"CanvasLayer/PrototypeEnemy/ComplianceBar"
 
 var current_int_line = 0
@@ -38,9 +36,6 @@ func _ready():
 	update_dialogue()
 	update_health_bar()
 
-#func _unhandled_input(event):
-	#if event.is_action_pressed("interact"):  # Press E
-		#start_battle()
 func _unhandled_input(event):
 	if not self.visible:
 		return  # Only allow inputs during battle
@@ -57,20 +52,6 @@ func _unhandled_input(event):
 	elif event.is_action_pressed("interact"):
 		start_battle()
 
-
-#func start_battle():
-	#print("Battle started!")
-	#enemy_health = MAX_HEALTH
-	#current_int_line = 0
-	#current_per_line = 0
-	#current_dialogue = []
-	#update_dialogue()
-	#update_health_bar()
-	#
-	#self.visible = true
-	#for child in get_children():
-		#child.visible = true
-		
 func start_battle():
 	print("Battle started!")
 	enemy_health = MAX_HEALTH
@@ -127,16 +108,6 @@ func update_dialogue():
 				dialogue_text.text = "End of PER dialogue."
 	else:
 		dialogue_text.text = "No dialogue available."
-
-#func _on_int_button_pressed():
-	#print("INT Button Pressed!")
-	#current_dialogue = int_dialogue
-	#current_int_line += 1
-	#if current_int_line < int_dialogue.size():
-		#update_dialogue()
-		#decrease_enemy_health(1)  # INT reduces by 1
-	#else:
-		#dialogue_text.text = "End of INT dialogue."
 		
 func _on_int_button_pressed():
 	print("INT Button Pressed!")
