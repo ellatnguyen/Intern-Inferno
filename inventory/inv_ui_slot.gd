a68cf431cc058f337d96ada6c65ec69d81259d53
Audrey Pan <panaudrey09@gmail.com>
2025-04-25 01:42:33 -0500

    items fit invent slot and closed clipboard added

--- inventory/inv_ui_slot.gd
+++ inventory/inv_ui_slot.gd
@@ -11,6 +11,9 @@ func update(slot:InvSlot):
 	else:
 		item_visual.visible=true
 		item_visual.texture=slot.item.texture
+		item_visual.scale = Vector2(0.8, 0.7)  # tweak this if needed
+		item_visual.centered = true
+		item_visual.position = Vector2.ZERO
 		if slot.amount > 1:
 			amount_text.visible=true
 		amount_text.text=str(slot.amount)
--- inventory/inventory_item.gd
+++ inventory/inventory_item.gd
@@ -4,3 +4,7 @@ class_name InvItem
 
 @export var name: String = ""
 @export var texture: Texture2D
+
+
+func _ready():
+	self.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
