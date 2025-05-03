a0a617b9bad6780413aaeaa58371b62a324e8f29
JDHarbs <athapa4@ku.edu>
2025-05-02 20:14:45 -0500

    Spider Enemy
    Began creating spider enemy

---
+++ Enemies/spider_enemy.gd
@@ -0,0 +1,12 @@
+# imp_enemy.gd
+extends "res://Enemies/base_enemy.gd"
+
+func _ready():
+	stats = {
+		"PER_DMG": 1,
+		"INT_DMG": 1,
+	}
+	dialogue_file_path = "res://Enemies/Dialogue/old_lady_spider.txt"
+	super._ready()
+	
+	dialogue_sprite = preload("res://Enemies/DialogueBattleSprites/spider.png")
